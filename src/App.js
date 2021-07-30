2ab536d9606cd5baded5f6c8915153aa8994d93c
SubhaSundaram <subhasundar664@gmail.com>
2021-07-30 16:39:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Editor from './components/Editor'
+import './components/Editor.css'
+import './App.css'
+
+function App() {
+  return (
+  <div className="App">
+    <Editor/>
+  </div>
+  )
+}
+
+export default App
